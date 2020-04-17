d649b221f9ed6fc59f1380d74ff14989f69b53d2
Nikolay Kostov <admin@nikolay.it>
2020-04-17 15:35:02 +0300

    Fix for possible null value for RadiantWin in PlayerMatch when the game is still in progress. Added documentation for all properties in PlayerMatch.

--- OpenDota.NET/Models/Players/PlayerMatch.cs
+++ OpenDota.NET/Models/Players/PlayerMatch.cs
@@ -5,67 +5,102 @@
 
     public class PlayerMatch
     {
-        // Match ID
+        /// <summary>
+        /// Gets or sets the match ID.
+        /// </summary>
         [JsonPropertyName("match_id")]
         public long MatchId { get; set; }
 
-        // Which slot the player is in. 0-127 are Radiant, 128-255 are Dire
+        /// <summary>
+        /// Gets or sets which slot the player is in. 0-127 are Radiant, 128-255 are Dire.
+        /// </summary>
         [JsonPropertyName("player_slot")]
         public int PlayerSlot { get; set; }
 
-        // Boolean indicating whether Radiant won the match
+        /// <summary>
+        /// Gets or sets whether Radiant won the match.
+        /// </summary>
         [JsonPropertyName("radiant_win")]
-        public bool RadiantWin { get; set; }
+        public bool? RadiantWin { get; set; }
 
-        // Duration of the game in seconds
+        /// <summary>
+        /// Gets or sets the duration of the game in seconds.
+        /// </summary>
         [JsonPropertyName("duration")]
         public int Duration { get; set; }
 
-        // Integer corresponding to game mode played. List of constants can be found here: https://github.com/odota/dotaconstants/blob/master/json/game_mode.json
+        /// <summary>
+        /// Gets or sets an integer corresponding to game mode played.
+        /// List of constants can be found here: https://github.com/odota/dotaconstants/blob/master/json/game_mode.json.
+        /// </summary>
         [JsonPropertyName("game_mode")]
         public int GameMode { get; set; }
 
-        // Integer corresponding to lobby type of match. List of constants can be found here: https://github.com/odota/dotaconstants/blob/master/json/lobby_type.json
+        /// <summary>
+        /// Gets or sets an integer corresponding to lobby type of match.
+        /// List of constants can be found here: https://github.com/odota/dotaconstants/blob/master/json/lobby_type.json.
+        /// </summary>
         [JsonPropertyName("lobby_type")]
         public int LobbyType { get; set; }
 
-        // The ID value of the hero played
+        /// <summary>
+        /// Gets or sets the ID value of the hero played.
+        /// </summary>
         [JsonPropertyName("hero_id")]
         public int HeroId { get; set; }
 
-        // Time the game started in seconds since 1970
+        /// <summary>
+        /// Gets or sets time the game started in seconds since 1970.
+        /// </summary>
         [JsonPropertyName("start_time")]
         public long StartTime { get; set; }
 
-        // Version
+        /// <summary>
+        /// Gets or sets the version.
+        /// </summary>
         [JsonPropertyName("version")]
         public int? Version { get; set; }
 
-        // Total kills the player had at the end of the game
+        /// <summary>
+        /// Gets or sets total kills the player had at the end of the game.
+        /// </summary>
         [JsonPropertyName("kills")]
         public int Kills { get; set; }
 
-        // Total deaths the player had at the end of the game
+        /// <summary>
+        /// Gets or sets total deaths the player had at the end of the game.
+        /// </summary>
         [JsonPropertyName("deaths")]
         public int Deaths { get; set; }
 
-        // Total assists the player had at the end of the game
+        /// <summary>
+        /// Gets or sets total assists the player had at the end of the game.
+        /// </summary>
         [JsonPropertyName("assists")]
         public int Assists { get; set; }
 
-        // Skill bracket assigned by Valve (Normal, High, Very High)
+        /// <summary>
+        /// Gets or sets skill bracket assigned by Valve (Normal, High, Very High).
+        /// </summary>
         [JsonPropertyName("skill")]
         public int? Skill { get; set; }
 
-        // Integer describing whether or not the player left the game. 0: didn't leave. 1: left safely. 2+: Abandoned
+        /// <summary>
+        /// Gets or sets integer describing whether or not the player left the game. 0: didn't leave. 1: left safely. 2+: Abandoned.
+        /// </summary>
         [JsonPropertyName("leaver_status")]
         public int LeaverStatus { get; set; }
 
-        // Size of the player's party
+        /// <summary>
+        /// Gets or sets size of the player's party.
+        /// </summary>
         [JsonPropertyName("party_size")]
         public int? PartySize { get; set; }
 
-        // Heroes (requires ?project=heroes)
+        /// <summary>
+        /// Gets or sets the heroes.
+        /// (requires ?project=heroes).
+        /// </summary>
         [JsonPropertyName("heroes")]
         public Dictionary<string, PlayerMatchHero> Heroes { get; set; }
     }
