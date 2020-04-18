8c8f4d0550054fdbefc1f6c8b7a95265381adbf4
Nikolay Kostov <admin@nikolay.it>
2020-04-18 11:53:52 +0300

    Fix for TestGetReplayData in ReplaysEndpointTests

--- src/OpenDota.NET.Tests/ReplaysEndpointTests.cs
+++ src/OpenDota.NET.Tests/ReplaysEndpointTests.cs
@@ -1,6 +1,5 @@
 ﻿namespace OpenDota.NET.Tests
 {
-    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -24,7 +23,9 @@
         [Fact]
         public async Task TestGetReplayData()
         {
-            var result = await this.openDotaApi.Replays.GetReplayDataAsync(new List<long> { 5360987052, 5361213941 });
+            var proMatches = await this.openDotaApi.ProMatches.GetProMatchesAsync();
+            var result =
+                await this.openDotaApi.Replays.GetReplayDataAsync(proMatches.Select(x => x.MatchId).Take(2).ToList());
             this.testOutputHelper.WriteLine(result.ToJsonString());
 
             Assert.Equal(2, result.Count);
